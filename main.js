b63bd0651e7e4dd22dc825a7595dff25b0553731
Neenus Gabriel <hello@neenus.com>
2021-05-23 23:51:50 -0400

    create video stream with JS

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("DOM Content Loaded");
+
+  const video = document.querySelector("#video");
+  // Check if device has a camera
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Use video without audio
+    const constraints = {
+      video: true,
+      audio: false,
+    };
+
+    //   // Start video stream
+    const startStream = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      video.srcObject = stream;
+    };
+
+    startStream();
+  }
 });
