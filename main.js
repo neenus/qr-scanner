c436f51d66f9501a96b9264cc429e6fbc6e2a4cf
Neenus Gabriel <hello@neenus.com>
2021-05-24 00:52:37 -0400

    implement barcode scanner functionality

--- main.js
+++ main.js
@@ -17,5 +17,29 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     startStream();
+
+    // Create new barcode detector
+    const barcodeDetector = new BarcodeDetector({ formats: ["qr_code"] });
+
+    // Detect code function
+    const detectCode = () => {
+      // Start detecting code on the video element
+      barcodeDetector
+        .detect(video)
+        .then((codes) => {
+          if (codes.length === 0) return;
+
+          for (const barcode of codes) {
+            alert(barcode.rawValue);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    };
+
+    setInterval(() => {
+      detectCode();
+    }, 1000);
   }
 });
